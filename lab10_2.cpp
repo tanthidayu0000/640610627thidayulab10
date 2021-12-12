60e8277731632d70966156069d7ccd8af8b4a198
thidayu <tanthidayu_0000@hotmail.com>
2021-12-12 13:39:50 +0700

    "lab10.2 finished"

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N, int M)
+{
+ if (N > 0 && M >0)
+ {
+  for (int i = 0; i < N; i++)
+  {
+   for (int j = 0; j < M; j++)
+   {
+    cout << "O";
+   }
+   cout <<  "\n";
+  }
+ }else{
+ cout << "Invalid input";}
+}
 
 int main(){
 	
